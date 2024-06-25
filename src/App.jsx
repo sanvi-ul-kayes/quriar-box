f39beacf7699e8872dc826b38f14c569d8c3f37a
abdullah al mridul <abdullah.github@zohomail.com>
2024-06-25 16:38:29 +0600

    abcd

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import Navber from './Navber'
-import Banner from './Banner'
-import Service from './Service'
-import Advertisements from './Advertisements'
-import Delivery from './Delivery'
-import Testimonials from './Testimonials'
-import Form from './Form'
-import Find from './Find'
-import Updates from './Updates'
-import Links from './Links'
-import Footer from './Footer'
-
+import React from "react";
+import Navber from "./Navber";
+import Banner from "./Banner";
+import Service from "./Service";
+import Advertisements from "./Advertisements";
+import Delivery from "./Delivery";
+import Testimonials from "./Testimonials";
+import Form from "./Form";
+import Find from "./Find";
+import Updates from "./Updates";
+import Links from "./Links";
+import Footer from "./Footer";
 
 const App = () => {
   return (
     <>
-      <Navber />
-      <Banner />
-      <Service />
-      <Advertisements />
-      <Delivery />
-      <Testimonials />
-      <Form />
-      <Find />
-      <Updates />
-      <Links />
-      <Footer/>
+      <div className={`font-["Raleway"]`}>
+        <Navber />
+        <Banner />
+        <Service />
+        <Advertisements />
+        <Delivery />
+        <Testimonials />
+        <Form />
+        <Find />
+        <Updates />
+        <Links />
+        <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Delivery.jsx
+++ src/Delivery.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React from "react";
 import { SlSocialYoutube } from "react-icons/sl";
-<SlSocialYoutube />
-
+<SlSocialYoutube />;
 
 const Delivery = () => {
-    return (
-        <div className='bg-[#FFD078] pt-[109px] pb-[202px]'>
-            <div className='w-container mx-auto'>
-                <div>
-                    <img className='relative after:top-0 after:left-0' src="./img/Rectangle 12.png" alt="Rectangle" />
-                    
-                    <SlSocialYoutube className='absolute' />
-                    <h2>FASTEST DELIVERY</h2>
-                    <p> You can get your valuable item in the fastest period of
- time with safety. Because your emergency
- is our first priority.</p>
-                </div>
-            </div>
-      </div>
-  )
-}
+  return (
+    <div className="bg-[#FFD078] pt-[109px] pb-[202px]">
+      <div className="w-container mx-auto">
+        <div className="relative">
+          <img
+            className="after:top-0 after:left-0"
+            src="./img/Rectangle 12.png"
+            alt="Rectangle"
+          />
 
-export default Delivery
+          <div className="absolute flex items-center justify-center top-0 w-full h-full">
+            <div className="flex flex-col items-center">
+              <SlSocialYoutube className="text-[#F95C19] h-[40px] mb-3 w-[40px]" />
+              <h2 className="text-[#F95C19] font-bold  text-[30px]">
+                FASTEST DELIVERY
+              </h2>
+              <p className="text-white text-[20px] mt-[10px] w-[70%] text-center">
+                {" "}
+                You can get your valuable item in the fastest period of time
+                with safety. Because your emergency is our first priority.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Delivery;
